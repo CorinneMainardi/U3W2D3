11f2faf72be61b37e512360fcbbdab07287b209d
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-10-16 16:58:14 +0200

    erroriiiiiiii

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { iProduct, iResponse } from '../../interface/i-product';
+import { ServiceProdService } from '../../services/service-prod.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  products: iProduct[] = [];
 
+  constructor(private prdSvc: ServiceProdService) {}
+
+  ngOnInit() {
+    this.prdSvc.getProduct().subscribe((products) => {
+      this.products = products.products;
+      console.log(this.products);
+    });
+  }
 }
